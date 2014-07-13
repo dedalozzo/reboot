6ab210f2f79b65e71ef6840c450f3e3535ea6e9a
Filippo Fadda <filippo.fadda@gmail.com>
2014-07-13 19:10:52 +0200

    added a pit.js

--- src/js/pit.js
+++ src/js/pit.js
@@ -1,3 +1,22 @@
 /**
  * Created by fff on 7/5/14.
  */
+
+//var ajax = 'ajax.programmazione.me';
+
+$(document).ready(
+  function() {
+    $("article > div.item-tools > a:first-child").click(
+      function() {
+        event.preventDefault();
+
+        var postId = $(this).parents("article").attr("id");
+        var like = $(this);
+
+        $.post('http://ajax.programmazione.me/vota/', {id: postId}, function(data) {
+          like.addClass('active');
+        });
+      }
+    );
+  }
+);
