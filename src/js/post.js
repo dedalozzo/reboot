e851d1a7f3a94e9f3dd043e40b595fb13ed08306
Filippo Fadda <filippo.fadda@gmail.com>
2015-05-13 00:04:42 +0200

    implemented many functions and fixed so many bugs

--- src/js/post.js
+++ src/js/post.js
@@ -3,7 +3,7 @@
 //! @author Filippo F. Fadda
 //! @copyright 3F s.a.s. di Filippo Fadda
 
-//var ajax = 'ajax.programmazione.me';
+var api = 'http://api.programmazione.me/';
 
 function like() {
   $("article > section.item-content > ul.item-buttons > li > button.btn-like").click(
@@ -15,29 +15,31 @@ function like() {
 
       $.ajax({
         type: "POST",
-        url: 'http://ajax.programmazione.me/like/',
+        url: api + 'like/',
         xhrFields: { withCredentials: true },
         dataType: "json",
         data: { id: postId },
+
         success: function(data) {
           score = like.closest('ul').find('button.score');
           votesCount = Number(score.text());
 
-          switch (data) {
-            case 1: // Registered
-              votesCount++;
-              like.addClass('active');
-              break;
-            case 2: // Deleted
-              votesCount--;
-              like.removeClass('active');
-              break;
-            case 3: // Replaced
-              // Can't occour in case of a Like.
-              break;
-            default: // Error
-              alert(data);
-          }
+          if (data[0])
+            switch (data[1]) {
+              case 1: // Registered
+                votesCount++;
+                like.addClass('active');
+                break;
+              case 2: // Deleted
+                votesCount--;
+                like.removeClass('active');
+                break;
+              case 3: // Replaced
+                // Can't occour in case of a Like.
+                break;
+            }
+          else
+            alert(data[1]);
 
           score.text(votesCount);
         }
@@ -49,32 +51,33 @@ function like() {
 
 
 function star() {
-  $("button.btn-star").click(
+  var btnStar = $("button.btn-star");
+
+  btnStar.click(
     function() {
       event.preventDefault();
 
       var postId = $("article").attr("id");
-      //var star = $(this);
 
       $.ajax({
         type: "POST",
-        url: 'http://ajax.programmazione.me/star/',
+        url: api + 'star/',
         xhrFields: { withCredentials: true },
         dataType: "json",
         data: { id: postId },
+
         success: function(data) {
-
-          switch (data) {
-            case 1: // Starred
-              $("button.btn-star").addClass('active');
-              break;
-            case 2: // Unstarred
-              $("button.btn-star").removeClass('active');
-              break;
-            default: // Error
-              alert(data);
-          }
-
+          if (data[0])
+            switch (data[1]) {
+              case 1: // Starred
+                btnStar.addClass('active');
+                break;
+              case 2: // Unstarred
+                btnStar.removeClass('active');
+                break;
+            }
+          else
+            alert(data[1]);
         }
       });
 
@@ -109,33 +112,25 @@ function revert() {
 
 
 function moveToTrash() {
-  $("button.btn-star").click(
+  $(document).on('click', '#m-trash',
     function() {
       event.preventDefault();
 
       var postId = $("article").attr("id");
-      //var star = $(this);
 
       $.ajax({
         type: "POST",
-        url: 'http://ajax.programmazione.me/move-to-trash/',
+        url: api + 'move-to-trash/',
         xhrFields: { withCredentials: true },
         dataType: "json",
         data: { id: postId },
+
         success: function(data) {
-
-          switch (data) {
-            case 1:
-              $("button.btn-star").addClass('active');
-              break;
-            case 2:
-              $("button.btn-star").removeClass('active');
-              break;
-            case -1:
-              // No user logged in
-              break;
+          if (data[0]) {
+            $("#b-admin").trigger('click');
           }
-
+          else
+            alert(data[1]);
         }
       });
 
@@ -145,33 +140,25 @@ function moveToTrash() {
 
 
 function restore() {
-  $("button.btn-star").click(
+  $(document).on('click', '#m-undo',
     function() {
       event.preventDefault();
 
       var postId = $("article").attr("id");
-      //var star = $(this);
 
       $.ajax({
         type: "POST",
-        url: 'http://ajax.programmazione.me/restore/',
+        url: api + 'restore/',
         xhrFields: { withCredentials: true },
         dataType: "json",
         data: { id: postId },
+
         success: function(data) {
-
-          switch (data) {
-            case 1:
-              $("button.btn-star").addClass('active');
-              break;
-            case 2:
-              $("button.btn-star").removeClass('active');
-              break;
-            case -1:
-              // No user logged in
-              break;
+          if (data[0]) {
+            $("#b-admin").trigger('click');
           }
-
+          else
+            alert(data[1]);
         }
       });
 
@@ -186,26 +173,185 @@ function markAsDraft() {
 
 
 function close() {
+  $(document).on('click', '#m-close',
+    function() {
+      event.preventDefault();
 
+      var postId = $("article").attr("id");
+
+      $.ajax({
+        type: "POST",
+        url: api + 'close/',
+        xhrFields: { withCredentials: true },
+        dataType: "json",
+        data: { id: postId },
+
+        success: function(data) {
+          if (data[0]) {
+            $("#b-admin").trigger('click');
+          }
+          else
+            alert(data[1]);
+        }
+      });
+
+    }
+  );
 }
 
 
 function lock() {
+  $(document).on('click', '#m-lock',
+    function() {
+      event.preventDefault();
 
+      var postId = $("article").attr("id");
+
+      $.ajax({
+        type: "POST",
+        url: api + 'lock/',
+        xhrFields: { withCredentials: true },
+        dataType: "json",
+        data: { id: postId },
+
+        success: function(data) {
+          if (data[0]) {
+            $("#b-admin").trigger('click');
+          }
+          else
+            alert(data[1]);
+        }
+      });
+
+    }
+  );
 }
 
 
 function unprotect() {
+  $(document).on('click', '#m-unprotect',
+    function() {
+      event.preventDefault();
 
+      var postId = $("article").attr("id");
+
+      $.ajax({
+        type: "POST",
+        url: api + 'unprotect/',
+        xhrFields: { withCredentials: true },
+        dataType: "json",
+        data: { id: postId },
+
+        success: function(data) {
+          if (data[0]) {
+            $("#b-admin").trigger('click');
+          }
+          else
+            alert(data[1]);
+        }
+      });
+
+    }
+  );
 }
 
 
 function hide() {
+  $(document).on('click', '#m-hide',
+    function() {
+      event.preventDefault();
 
+      var postId = $("article").attr("id");
+
+      $.ajax({
+        type: "POST",
+        url: api + 'hide/',
+        xhrFields: { withCredentials: true },
+        dataType: "json",
+        data: { id: postId },
+
+        success: function(data) {
+          if (data[0]) {
+            $("#b-admin").trigger('click');
+          }
+          else
+            alert(data[1]);
+        }
+      });
+
+    }
+  );
 }
 
 
 function show() {
+  $(document).on('click', '#m-show',
+    function() {
+      event.preventDefault();
+
+      var postId = $("article").attr("id");
+
+      $.ajax({
+        type: "POST",
+        url: api + 'show/',
+        xhrFields: { withCredentials: true },
+        dataType: "json",
+        data: { id: postId },
+
+        success: function(data) {
+          if (data[0]) {
+            $("#b-admin").trigger('click');
+          }
+          else
+            alert(data[1]);
+        }
+      });
+
+    }
+  );
+}
+
+
+function buildToolsMenu() {
+
+  $(document).on('click', '#b-admin',
+    function() {
+      event.preventDefault();
+      var postId = $(this).parents("article").attr("id");
+      var pippo = $("#dropdown-moderator");
+
+      if (pippo.css('display') == 'block') {
+        pippo.load("http://programmazione.me/ajax/moderator-menu/", {id: postId},
+          function() {
+            var dropdown = $('.dropdown:visible').eq(0),
+              trigger = dropdown.data('dropdown-trigger'),
+              hOffset = trigger ? parseInt(trigger.attr('data-horizontal-offset') || 0, 10) : null,
+              vOffset = trigger ? parseInt(trigger.attr('data-vertical-offset') || 0, 10) : null;
+
+            if (dropdown.length === 0 || !trigger) return;
+
+            // Position the dropdown relative-to-parent...
+            if (dropdown.hasClass('dropdown-relative')) {
+              dropdown.css({
+                left: dropdown.hasClass('dropdown-anchor-right') ?
+                trigger.position().left - (dropdown.outerWidth(true) - trigger.outerWidth(true)) - parseInt(trigger.css('margin-right'), 10) + hOffset :
+                trigger.position().left + parseInt(trigger.css('margin-left'), 10) + hOffset,
+                top: trigger.position().top + trigger.outerHeight(true) - parseInt(trigger.css('margin-top'), 10) + vOffset
+              });
+            } else {
+              // ...or relative to document
+              dropdown.css({
+                left: dropdown.hasClass('dropdown-anchor-right') ?
+                trigger.offset().left - (dropdown.outerWidth() - trigger.outerWidth()) + hOffset : trigger.offset().left + hOffset,
+                top: trigger.offset().top + trigger.outerHeight() + vOffset
+              });
+            }
+
+          });
+      }
+      else
+        pippo.empty();
+    });
 
 }
 
@@ -227,5 +373,6 @@ $(document).ready(
     unprotect();
     hide();
     show();
+    buildToolsMenu();
   }
 );
