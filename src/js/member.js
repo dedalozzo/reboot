73657beab6c13fc92743f5045b8250aaedd6ec8d
Filippo Fadda <filippo.fadda@gmail.com>
2016-04-28 20:59:02 +0200

    added a script member.js

---
+++ src/js/member.js
@@ -0,0 +1,48 @@
+//! @file member.js
+//! @brief AJAX requests for a list of members.
+//! @author Filippo F. Fadda
+//! @copyright REINDEX LTD
+
+var api = 'http://api.programmazione.me/';
+
+
+function like() {
+  $("article > section.item-content > div > div > button.btn-icon").click(
+    function() {
+      event.preventDefault();
+
+      var memberId = $(this).parents("article").attr("id");
+      var connect = $(this);
+
+      $.ajax({
+        type: "POST",
+        url: api + 'connect/',
+        xhrFields: { withCredentials: true },
+        dataType: "json",
+        data: { id: memberId },
+
+        success: function(data) {
+          if (data[0])
+            switch (data[1]) {
+              case 1: // Connected
+                connect.text('CONNECTED');
+                break;
+              case 2: // Unconnected
+                connect.text('UNCONNECTED');
+                break;
+            }
+          else
+            alert(data[1]);
+        }
+      });
+
+    }
+  );
+}
+
+
+$(document).ready(
+  function() {
+    connect();
+  }
+);
